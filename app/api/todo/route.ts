8b18750543cfb0b95fdd22cab39311fc0925521e
mfareed1947 <fareed1504@gmail.com>
2023-05-22 01:15:45 +0500

    create a frontend of get and post method

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,3 +1,33 @@
-export async function GET(request: Request) {
-    return new Response('Hello, Next.js!')
+import { NextRequest, NextResponse } from "next/server";
+import { db, todoTable} from "../../lib/drizzle"
+import { sql } from "@vercel/postgres";
+
+export async function GET(request: NextRequest) {
+
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS todos(id SERIAL , task VARCHAR(255))`;
+    const res = await db.select().from(todoTable);
+    return NextResponse.json({ data: res })
+  } catch (error) {
+    console.log((error as { message: string }).message)
+    return NextResponse.json({ message: 'apna kaam sahi se check karo' })
   }
+}
+
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+
+  try {
+    if (req.task) {
+      const res = await db.insert(todoTable).values({
+        task: req.task,
+      }).returning(); 
+      return NextResponse.json({ message: 'Data added successfully' })
+    } else {
+      throw new Error("task field is required");
+    }
+  } catch (error) {
+    return NextResponse.json({ message: (error as { message: string }).message })
+  }
+}
