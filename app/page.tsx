4ebd4071e12d5aa3d79e8523cb1ecdd30411cb2e
mfareed1947 <fareed1504@gmail.com>
2023-05-22 01:32:58 +0500

    create a frontend of get and post method

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className='bg-gradient-to-tr from-primary to-secondary h-screen w-full flex justify-center items-center shadow-lg'>
       <div className='px-3 py-4 max-w-md w-full bg-gradient-to-br from-white/30 to-white/50 backdrop-blur-xl rounded-xl'>
         {/* @ts-ignore */}
-        <TodoList />
+        <TodoList  />
         <AddTodo />
         <div className='w-1/2 h-1.5 bg-black/80 mx-auto rounded-lg '></div>
       </div>
