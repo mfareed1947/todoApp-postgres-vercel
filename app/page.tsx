34576d63f4352f83c0333bb9abd5558988fef3f5
mfareed1947 <fareed1504@gmail.com>
2023-05-22 23:10:36 +0500

    create again frontend with database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image'
+import AddTodo from '@/Components/AddTodo'
+import TodoList from '@/Components/TodoList'
 
 export default function Home() {
   return (
-    <h1>Hi ! I`m Muhammad Fareed</h1>
+    <main className='bg-gradient-to-tr from-primary to-secondary h-screen w-full flex justify-center items-center shadow-lg'>
+    <div className='px-3 py-4 max-w-md w-full bg-gradient-to-br from-white/30 to-white/50 backdrop-blur-xl rounded-xl'>
+      {/* @ts-ignore */}
+      <TodoList />
+      <AddTodo />
+      <div className='w-1/2 h-1.5 bg-black/80 mx-auto rounded-lg '></div>
+    </div>
+  </main>
   )
 }
